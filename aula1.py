d2319366d69df490a31eaa3eee365c8bdf63b7b1
Isaque <isaque.cardoso.pereira@escola.pr.gov.br>
2025-02-21 09:29:43 -0300

    adicionou parada

--- aula1.py
+++ aula1.py
@@ -1,6 +1,17 @@
 import random
-
 sorteio = random.randint(1, 10)
-chute = input("Digite o seu chute:")
-print(sorteio)
-print(chute)
+print("### JOGO DA ADIVINHAÇÃO ###")
+print("Adivinhe o número que estou pensando")
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print("Tentativa", tentativa)
+    chute = int(imput("Digite seu chute:"))
+    if (chute == sorteio):
+        print("Parabéns, você acertou!")
+        break
+    elif (chute > sorteio):
+        print("Chute um numero menor!")
+    elif (chute < sorteio):
+        print("Chute um numero maior!")
+    tentativa = tentativa + 1
